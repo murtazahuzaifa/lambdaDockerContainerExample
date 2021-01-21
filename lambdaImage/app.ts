07b85ddbd551b946ea2d7741e6086947f37da609
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-01-22 04:25:12 +0500

    make node server to run in lambda docker container

--- lambdaImage/app.ts
+++ lambdaImage/app.ts
@@ -1,31 +1,43 @@
 import { APIGatewayProxyEvent, Context, Callback } from "aws-lambda";
 // const randomWords = require("random-words");
 // import * as fs from 'fs';
-// import axios from 'axios';
+const axios = require('axios');
 import { exec } from 'child_process';
 
-exports.handler = async (event: any, context: Context, callback: Callback) => {
+type Event = { delay?: number, cmd?: string, hitUrl?: string, killId?: string };
+
+export const handler = async (event: Event, context: Context, callback: Callback) => {
+  // starting nodejs http server
+  exec(`nohup node server.js > /tmp/output.log &`, (error, stdout, stderr) => { //uname -svr kill 20 21 22 32 43 54
+    if (error) { console.log(`error: ${error.message}`); return; }
+    if (stderr) { console.log(`stderr: ${stderr}`); return; }
+    console.log(`\nstdout-1==>: ${stdout}\n`);
+  });
+  await delay(event.delay || 1000) // delay
   console.log("Event==>", event);
 
   // reading file form file-system
   // const txt = fs.readFileSync('./file.txt', 'utf-8');
   // console.log(txt);
 
-  // exec(`nohup node server.js > output.log &`, (error, stdout, stderr) => { //uname -svr
-  //   if (error) { console.log(`error: ${error.message}`); return; }
-  //   if (stderr) { console.log(`stderr: ${stderr}`); return; }
-  //   console.log(`\nstdout-1==>: ${stdout}\n`);
-  // });
 
-  exec(event.cmd||'pwd', (error, stdout, stderr) => { //uname -svr
+  // setTimeout(async () => {
+
+
+  // }, 1000)
+
+  exec(event.cmd || 'pwd', (error, stdout, stderr) => { //uname -svr
     if (error) { console.log(`error: ${error.message}`); return; }
     if (stderr) { console.log(`stderr: ${stderr}`); return; }
     console.log(`\nstdout-2==>: ${stdout}\n`);
   });
-
   // getting response from node server
-  // const res = await axios.get('http://localhost:5300');//http://localhost:5300
-  // console.log("AXIOS res ===>", res);
+  if (event.hitUrl) {
+    axios.default.get('http://localhost:5300')
+      .then((e: any) => { console.log("AXIOS RESPONSE ===>", e.data); event.killId && exec(event.killId) })
+      .catch((e: any) => { console.log("AXIOS ERROR ===>", e.response); event.killId && exec(event.killId) })
+  }
+
 
   // Generating random word
   const myWord = "hello world" //randomWords();
@@ -65,3 +77,6 @@ const Responses = {
     };
   },
 };
+
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))

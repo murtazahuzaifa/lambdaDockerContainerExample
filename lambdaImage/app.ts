ea6a76b58ebf7b22216ed9b0e77a2b997407071a
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-01-23 23:11:47 +0500

    grafana server init inside lambda container

--- lambdaImage/app.ts
+++ lambdaImage/app.ts
@@ -1,19 +1,19 @@
 import { APIGatewayProxyEvent, Context, Callback } from "aws-lambda";
 // const randomWords = require("random-words");
-// import * as fs from 'fs';
+import * as fs from 'fs';
 const axios = require('axios');
 import { exec } from 'child_process';
 
-type Event = { delay?: number, cmd?: string, hitUrl?: string, killId?: string };
+type Event = { delay?: number, cmd?: string, hitUrl?: string, read?: string, write?: string };
 
-exec(`nohup node server.js > /tmp/output.log &`, (error, stdout, stderr) => { //uname -svr kill 20 21 22 32 43 54 | pkill -f server.js
+// starting nodejs http server
+exec(`nohup ./bin/grafana-server --homepath="./" --config="./conf/defaults.ini" > /tmp/output.log &`, (error, stdout, stderr) => { //uname -svr kill 20 21 22 32 43 54 | pkill -f server.js
   if (error) { console.log(`error: ${error.message}`); return; }
   if (stderr) { console.log(`stderr: ${stderr}`); return; }
   console.log(`\nstdout-1==>: ${stdout}\n`);
 });
 
 export const handler = async (event: Event, context: Context, callback: Callback) => {
-  // starting nodejs http server
   // await delay(event.delay || 1000) // delay
   console.log("Event==>", event);
 
@@ -25,9 +25,15 @@ export const handler = async (event: Event, context: Context, callback: Callback
 
   // getting response from node server
   if (event.hitUrl) {
-    await http('http://localhost:5300');
+    await http('http://localhost:3000');
   }
 
+
+  const filePath = '/tmp/file.txt';
+  if (event.read) { console.log("Reading File==> ", fs.readFileSync(filePath, 'utf-8')); }
+  if (event.write) { fs.appendFileSync(filePath, event.write, { encoding: "utf-8" }); console.log('File Written') }
+
+  await delay(event.delay || 1000) // delay
 
   return Responses._200({ response: "hello world" });
   // callback(null, Responses._200({ myWord }));
@@ -83,3 +89,7 @@ const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 "cmd": "ps -ef",
 "killId": "pkill -f server.js",
 */
+// /mnt/lambda/function
+// ./grafana/bin/grafana-server --homepath="./grafana" --config="./grafana/conf/defaults.ini"
+// nohup node server.js > /tmp/output.log &
+// nohup ./grafana/bin/grafana-server --homepath="./grafana" --config="./grafana/conf/defaults.ini" > /tmp/output.log &

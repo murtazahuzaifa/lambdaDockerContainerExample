dd51e29b58899d9ebf289a765e7bb570e583b167
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-02-12 20:30:33 +0500

    echo statement commented in install-grafana.sh file

--- grafana-installation/install-grafana.sh
+++ grafana-installation/install-grafana.sh
@@ -6,13 +6,13 @@ conf_file="grafana.ini"
 cd $1
 
 #installing grafana
-echo "Intalling $VERSION ..."
+# echo -e "Intalling $VERSION ..."
 wget https://dl.grafana.com/oss/release/$VERSION.linux-amd64.tar.gz
 tar -zxvf $VERSION.linux-amd64.tar.gz
 
 
 #installing plugin
-echo "Intalling plugin ..."
+# echo "Intalling plugin ..."
 $VERSION/bin/grafana-cli --pluginsDir "./$VERSION/plugins" plugins install grafana-timestream-datasource
 
 mv $VERSION grafana #renaming the folder
@@ -20,4 +20,4 @@ mv $VERSION grafana #renaming the folder
 #removing the .tar.gz file
 rm $VERSION.linux-amd64.tar.gz
 
-echo "$VERSION Intallation Done."
+# echo "$VERSION Intallation Done."
